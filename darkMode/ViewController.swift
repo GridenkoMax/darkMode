3b37d1654422834858924968d575f54d6f127099
Maksim <maksgridenko@ya.ru>
2023-09-27 20:37:01 +0500

    no message

--- darkMode/ViewController.swift
+++ darkMode/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var lbl: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        if #available(iOS 12.0, *){
+            
+            if self.traitCollection.userInterfaceStyle == .dark{
+                self.lbl.text = "Dark mode"
+            } else {
+                self.lbl.text = "Light mode"
+            }
+        }
+    }
 
 }
-
